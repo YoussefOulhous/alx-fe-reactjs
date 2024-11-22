95ee22536bdb04ce934b4ee13da6135269405ccc
Youssef <youssefouhous32@gmail.com>
2024-11-22 14:18:26 +0100

    rt

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 
 function RegistrationForm () {
 
-    const [name,setname] = useState('');
+    const [username,setusername] = useState('');
     const [email,setemail] = useState('');
     const [password,setpassword] = useState('');
     const [error,setError] = useState('')
 
     const handleSubmit = (e) => { e.preventDefault() ;
 
-    if(!name || !email || !password ) {
+    if(!username || !email || !password ) {
         setError('please check if you enter all you infos !!')
         return ;
     } 
 
     setError(""); 
-    alert(`Form submitted: Name: ${name}, Email: ${email}`);
+    alert(`Form submitted: Name: ${username}, Email: ${email}`);
     
   };
 
@@ -28,7 +28,7 @@ function RegistrationForm () {
             
             <label>Name :</label>
             <input type="text" 
-                value={name}
+                value={username}
                 
                 onChange={(e) => setname(e.target.value)} 
              />
